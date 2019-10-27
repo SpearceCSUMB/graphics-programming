7f371710fcad50adea1dcad58368ae81839b37a1
SpearceCSUMB <44475801+SpearceCSUMB@users.noreply.github.com>
2019-10-27 14:18:05 -0700

    Added Authors names

--- Module 1/Vectors/math/vector3.js
+++ Module 1/Vectors/math/vector3.js
@@ -1,4 +1,5 @@
 /*
+ * Authors: Kevin Bentley, Samuel Pearce
  * An "object" representing a 3d vector to make operations simple and concise.
  *
  * Similar to how we work with plain numbers, we will work with vectors as
