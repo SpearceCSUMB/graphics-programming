0a48c2c8518ea2eab6916542aae2a81209bbf622
Kevin Bentley <kbentley@cognitics.net>
2019-10-29 07:57:16 -0600

    Up through Step 4 in the assignment completed.

---
+++ Module 2/Raytracer/math/sphere.js
@@ -0,0 +1,142 @@
+/*
+ * Authors: Kevin Bentley, Samuel Pearce
+ * An object type representing an implicit sphere.
+ *
+ * @param center A Vector3 object representing the position of the center of the sphere
+ * @param radius A Number representing the radius of the sphere.
+ */
+
+var Sphere = function(center, radius) {
+  if (!(this instanceof Sphere)) {
+    console.error("Sphere constructor must be called with the new operator");
+  }
+
+  if (!(center instanceof Vector3)) {
+    console.error("The sphere center must be a Vector3");
+  }
+
+  if ((typeof(radius) != 'number')) {
+    console.error("The radius must be a Number");
+  }
+
+  this.center = center;
+  this.radius = radius;
+
+  if (center === undefined) {
+    this.center = new Vector3(0, 0, 0);
+  }
+
+  if (radius === undefined) {
+    this.radius = 1;
+  }
+
+
+  // todo - make sure center and radius are replaced with default values if and only if they
+  // are invalid or undefined (i.e. center should be of type Vector3 & radius should be a Number)
+  // - the default center should be the zero vector
+  // - the default radius should be 1
+
+  this.raycast = function(ray) {
+    
+    //Quick test for inside sphere
+    var raySphereDistancSqr = ray.origin.minus(this.center).lengthSqr();
+    if(raySphereDistancSqr < (this.radius*this.radius))
+    {
+      var result = {
+        hit: false
+      };
+      return result;
+    }
+
+    var a = ray.direction.dot(ray.direction);
+    var b = ray.direction.times(2).dot(ray.origin.minus(this.center));
+    var c = ray.origin.minus(this.center).dot((ray.origin.minus(this.center))) - (this.radius * this.radius);
+
+    var alpha1 = 0;
+    var alpha2 = 0;
+    var discriminant = ((b*b) - (4*a*c));
+    
+    if(discriminant < 0)
+    {
+      //No intersections
+      var result = {
+        hit: false
+      };
+      return result;
+    }
+    var numerator = (-b - Math.sqrt(discriminant));
+    var denominator = (2*a);
+    alpha1 = numerator / denominator;
+    var intersectionPoint = ray.origin.plus((ray.direction.times(alpha1)));
+    normal = intersectionPoint.minus(this.center);
+    normal.normalize();
+
+    //Tangent intersection
+    if (discriminant==0)
+    {      
+      numerator = -b;
+      denominator = (2*a);
+      var alpha = numerator / denominator;
+      if(alpha < 0)
+      {
+        //Negative intersections
+        var result = {
+        hit: false
+        }
+        return result;
+      };
+      intersectionPoint = ray.origin.plus((ray.direction.times(alpha)));
+      normal = intersectionPoint.minus(this.center);
+      normal.normalize();
+      var result = {
+        hit: true,
+        point: intersectionPoint,
+        normal: normal,
+        distance: alpha
+      };
+      return result;
+    }
+    if (discriminant > 0) {
+      //2 Intersections
+      discriminant = (Math.pow(b, 2) - (4*a*c));
+      numerator = (-b + Math.sqrt(discriminant));
+      denominator = (2*a);
+      alpha2 = numerator / denominator;
+      var alpha = alpha2;
+      if(alpha1 < alpha2)
+      {
+        discriminant = (Math.pow(b, 2) - (4*a*c));
+        numerator = (-b - Math.sqrt(discriminant));
+        denominator = (2*a);
+        alpha = numerator / denominator;        
+      }
+      if(alpha < 0)
+      {
+        //Negative intersections
+        var result = {
+        hit: false
+      };
+      return result;
+      }
+      intersectionPoint = ray.origin.plus((ray.direction.times(alpha)));
+      normal = intersectionPoint.minus(this.center);
+      normal.normalize();
+      var result = {
+        hit: true,
+        point: intersectionPoint,
+        normal: normal,
+        distance: alpha
+      };
+      return result;
+      
+      
+    }else{
+      //No intersections
+      var result = {
+        hit: false
+      };
+      return result;
+    }
+    return result;
+  }
+};
---
+++ Module 2/Raytracer/math/vector3.js
@@ -0,0 +1,146 @@
+/*
+ * Authors: Kevin Bentley, Samuel Pearce
+ * An "object" representing a 3d vector to make operations simple and concise.
+ *
+ * Similar to how we work with plain numbers, we will work with vectors as
+ * an entity unto itself.  Note the syntax below: var Vector3 = function...
+ * This is different than you might be used to in most programming languages.
+ * Here, the function is meant to be instantiated rather than called and the
+ * instantiation process IS similar to other object oriented languages => new Vector3()
+ */
+
+var Vector3 = function(x, y, z) {
+  this.x = x; this.y = y; this.z = z;
+
+  // Sanity check to prevent accidentally using this as a normal function call
+  if (!(this instanceof Vector3)) {
+    console.error("Vector3 constructor must be called with the new operator");
+  }
+
+  // todo - make sure to set a default value in case x, y, or z is not passed in
+  if (this.x == null && this.y == null && this.z == null) {
+    this.x = 0; this.y = 0; this.z = 0;
+  }
+
+  
+
+  this.set = function(x, y, z) {
+    // todo set 'this' object's values to those from x, y, and z
+    this.x = x; this.y = y; this.z = z;
+    return this;
+  }
+
+  this.clone = function() {
+    return new Vector3(this.x, this.y, this.z);
+  };
+
+  this.copy = function(other) {
+    // copy the values from other into 'this'
+    this.x = other.x; this.y = other.y; this.z = other.z;
+    return this;
+  }
+
+  this.add = function(v) {
+    // todo - add v to 'this' vector
+    // This SHOULD change the values of this.x, this.y, and this.z
+    this.x = this.x + v.x;
+    this.y = this.y + v.y;
+    this.z = this.z + v.z;
+    return this;
+  };
+
+  this.subtract = function(v) {
+    // todo - subtract v from 'this' vector
+    // This SHOULD change the values of this.x, this.y, and this.z
+    this.x = this.x - v.x;
+    this.y = this.y - v.y;
+    this.z = this.z - v.z;
+    return this;
+  };
+
+  this.negate = function() {
+    // multiply 'this' vector by -1
+    // This SHOULD change the values of this.x, this.y, and this.z
+    this.x = this.x * -1;
+    this.y = this.y * -1;
+    this.z = this.z * -1;
+    return this;
+  };
+
+  this.multiplyScalar = function(scalar) {
+    // multiply 'this' vector by "scalar"
+    // This SHOULD change the values of this.x, this.y, and this.z
+    this.x = this.x*scalar;
+    this.y = this.y*scalar;
+    this.z = this.z*scalar;
+    return this;
+  };
+
+  this.length = function() {
+    // todo - return the magnitude (A.K.A. length) of 'this' vector
+    // This should NOT change the values of this.x, this.y, and this.z
+    var mag = 0;
+    mag = Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2) + Math.pow(this.z,2));
+    return mag;
+  };
+
+  this.lengthSqr = function() {
+    // todo - return the squared magnitude of this vector ||v||^2
+    // This should NOT change the values of this.x, this.y, and this.z
+    var mag = 0;
+    mag = Math.pow(this.x,2) + Math.pow(this.y,2) + Math.pow(this.z,2);
+    return mag;
+  };
+
+  this.normalized = function() {
+    // todo - return a new vector instance that is a normalized clone of 'this' vector
+    // This should NOT change the values of this.x, this.y, and this.z
+    normal = new Vector3(this.x, this.y, this.z);
+    var mag = normal.length();
+    normal.x = normal.x / mag;
+    normal.y = normal.y / mag;
+    normal.z = normal.z / mag;
+    return normal; // Should return a new vector that is not this
+  };
+
+  this.normalize = function() {
+    // todo - Change the components of this vector so that its magnitude will equal 1.
+    // This SHOULD change the values of this.x, this.y, and this.z
+    var mag = 0;
+    mag = Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2) + Math.pow(this.z,2));
+    this.x = this.x / mag;
+    this.y = this.y / mag;
+    this.z = this.z / mag;
+    return this;
+  };
+
+  this.dot = function(other) {
+    // todo - return the dot product between this vector and "other"
+    // This should NOT change the values of this.x, this.y, and this.z
+    var dp = 0;
+    dp = (this.x * other.x) + (this.y * other.y) + (this.z * other.z);
+    return dp;
+  };
+
+  // Convenience function, so we can do operations like regular math instead of modifying in place
+  this.minus = function(other) {
+    var newVec = this.clone();
+    newVec.subtract(other);
+    return newVec;
+  }
+
+  // Convenience function, so we can do operations like regular math instead of modifying in place
+  this.plus = function(other) {
+    var newVec = this.clone();
+    newVec.add(other);
+    return newVec;
+  }
+
+  // Convenience function, so we can do operations like regular math instead of modifying in place
+  this.times = function(other) {
+    var newVec = this.clone();
+    newVec.multiplyScalar(other);
+    return newVec;
+  }
+
+};
